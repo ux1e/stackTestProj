df613045ea6a71e072b1da2ca1bd2dd061d8106d
Иван <91054109+ux1e@users.noreply.github.com>
2024-07-12 21:35:38 +0300

    переработан метод select_value_by_house_and_month

--- select_value_by_house_and_month.sql
+++ select_value_by_house_and_month.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION stack.select_value_by_house_and_month(
-    IN house_number INT,
-    IN input_month DATE
+    house_number INT,
+    input_month DATE
 )
 RETURNS TABLE (
     acc INT,
@@ -11,22 +11,22 @@ AS $$
 BEGIN
   RETURN QUERY
   SELECT
-    a.number,
+    a.number AS acc,
     c.name,
-    COALESCE((
-      SELECT mp.value::int
-      FROM stack.Meter_Pok mp
-      WHERE mp.month = date_trunc('month', input_month)
-        AND mp.counter_id = c.row_id
-        AND mp.acc_id = a.row_id
-      ORDER BY mp.date DESC
-      LIMIT 1
-    ), 0) AS last_value
-  FROM stack.Accounts AS h
-  JOIN stack.Accounts AS a ON h.row_id = a.parent_id
-  JOIN stack.Counters AS c ON a.row_id = c.acc_id
-  WHERE h.type = 1
-    AND h.number = house_number
-    AND a.type = 3;
+    COALESCE(
+      (SELECT mp.value
+       FROM stack.Meter_Pok mp
+       WHERE 
+         mp.month = date_trunc('month', input_month)
+         AND mp.acc_id = a.row_id
+         AND mp.counter_id = c.row_id
+       ORDER BY mp.date DESC
+       LIMIT 1), 
+    0) AS value
+  FROM stack.Accounts a
+  JOIN stack.Counters c ON a.row_id = c.acc_id
+  WHERE 
+    a.type = 3  -- Лицевые счета
+    AND a.parent_id = (SELECT row_id FROM stack.Accounts WHERE number = house_number AND type = 1);
 END;
 $$ LANGUAGE plpgsql;
